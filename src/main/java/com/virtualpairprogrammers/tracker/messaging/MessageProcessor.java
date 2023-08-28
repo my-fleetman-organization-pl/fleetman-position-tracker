de15f22a8d33acae7f852d661d9a70112c00a6ed
pals <pals@gft.com>
2023-08-28 12:20:09 +0200

    break

--- src/main/java/com/virtualpairprogrammers/tracker/messaging/MessageProcessor.java
+++ src/main/java/com/virtualpairprogrammers/tracker/messaging/MessageProcessor.java
@@ -33,7 +33,8 @@ public class MessageProcessor {
 				                          .withLat(new BigDecimal(incomingMessage.get("lat")))
 				                          .withLng(new BigDecimal(incomingMessage.get("long")))
 				                          .withTimestamp(convertedDatestamp)
-				                          .build();
+				                          .withSpeed(47.5)
+										  .build();
 				                          
 		data.updatePosition(newReport);
 	}
